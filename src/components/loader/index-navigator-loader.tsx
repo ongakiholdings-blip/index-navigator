0294b465deb0e3fab5c1124bbaf99b3db061af8a
ongakiholdings-blip <ongakiholdings@gmail.com>
2026-09-09 11:48:29 +0300

    commit

--- src/components/loader/index-navigator-loader.tsx
+++ src/components/loader/index-navigator-loader.tsx
@@ -5,13 +5,18 @@ type TIndexNavigatorLoaderProps = {
 };
 
 const ECOSYSTEM_APPS = [
-    { name: 'YouTube', glyph: '▶', className: 'youtube' },
-    { name: 'TikTok', glyph: '♪', className: 'tiktok' },
-    { name: 'WhatsApp', glyph: '◔', className: 'whatsapp' },
-    { name: 'Telegram', glyph: '➤', className: 'telegram' },
-    { name: 'Instagram', glyph: '◎', className: 'instagram' },
-    { name: 'Deriv', glyph: 'D', className: 'deriv' },
-    { name: 'MT5', glyph: '5', className: 'mt5' },
+    { name: 'YouTube', glyph: '▶', className: 'youtube', href: 'https://www.youtube.com/@index_navigatorke' },
+    { name: 'TikTok', glyph: '♪', className: 'tiktok', href: 'https://www.tiktok.com/@indexnavigator' },
+    { name: 'WhatsApp', glyph: '◔', className: 'whatsapp', href: 'https://wa.me/+254115335502' },
+    { name: 'Telegram', glyph: '➤', className: 'telegram', href: 'https://t.me/index_navigator' },
+    {
+        name: 'Instagram',
+        glyph: '◎',
+        className: 'instagram',
+        href: 'https://www.instagram.com/index_navigator?igsi=eDE0Yzg3Z2swZHEz',
+    },
+    { name: 'Deriv', glyph: 'D', className: 'deriv', href: 'https://app.deriv.com' },
+    { name: 'MT5', glyph: '5', className: 'mt5', href: 'https://mt5.deriv.com' },
 ];
 
 const IndexNavigatorLoader = ({ isInitializing = true }: TIndexNavigatorLoaderProps) => (
@@ -25,20 +30,27 @@ const IndexNavigatorLoader = ({ isInitializing = true }: TIndexNavigatorLoaderPr
             <span className='index-navigator-loader__orb-layer index-navigator-loader__orb-layer--one' />
             <span className='index-navigator-loader__orb-layer index-navigator-loader__orb-layer--two' />
             <span className='index-navigator-loader__orb-layer index-navigator-loader__orb-layer--three' />
+            <span className='index-navigator-loader__orb-waterline' />
+            <span className='index-navigator-loader__orb-bubbles index-navigator-loader__orb-bubbles--one' />
+            <span className='index-navigator-loader__orb-bubbles index-navigator-loader__orb-bubbles--two' />
             <span className='index-navigator-loader__orb-glow' />
             <span className='index-navigator-loader__orb-pulse index-navigator-loader__orb-pulse--one' />
             <span className='index-navigator-loader__orb-pulse index-navigator-loader__orb-pulse--two' />
             </div>
-            <div className='index-navigator-loader__ecosystem' aria-hidden='true'>
-            {ECOSYSTEM_APPS.map(({ name, glyph, className }, index) => (
-                <span
+            <div className='index-navigator-loader__ecosystem'>
+            {ECOSYSTEM_APPS.map(({ name, glyph, className, href }, index) => (
+                <a
                     key={name}
                     className={`index-navigator-loader__app index-navigator-loader__app--${className}`}
+                    href={href}
+                    target='_blank'
+                    rel='noreferrer'
+                    aria-label={`Open ${name}`}
                     style={{ '--app-index': index } as React.CSSProperties}
                 >
                     <b>{glyph}</b>
                     <small>{name}</small>
-                </span>
+                </a>
             ))}
             </div>
             <h1>Loading<span>.</span><span>.</span><span>.</span></h1>
